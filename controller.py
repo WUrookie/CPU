96ff47838a354f46db92e4fc4d445b2805a73a04
wxm <473570721@qq.c>
2022-06-03 11:48:46 +0800

    fetch

---
+++ controller.py
@@ -0,0 +1,26 @@
+
+
+import os
+import pin
+import assembly as ASM
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, "micro.bin")
+
+micro = [pin.HLT for _ in range(0x10000)]
+
+for addr in range(0x10000):
+    ir = addr >> 8
+    psw = (addr >> 4) & 0xf
+    cyc = addr & 0xf
+    
+    if(cyc < len(ASM.FETCH)):
+        micro[addr] = ASM.FETCH[cyc]
+
+with open(filename, 'wb') as file:
+    for val in micro:
+        value = val.to_bytes(4, byteorder='little')
+        file.write(value)
+
+
+print('compile micor finished !!')
